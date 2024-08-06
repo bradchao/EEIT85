d5511169f632c6cda1f2d51608703603e72e908e
User <User@10.0.100.157>
2024-08-06 11:50:51 +0800

    08051150

--- BradJava/src/tw/brad/tutor/MySign.java
+++ BradJava/src/tw/brad/tutor/MySign.java
@@ -1,9 +1,30 @@
 package tw.brad.tutor;
 
-public class MySign {
+import java.awt.BorderLayout;
 
+import javax.swing.JFrame;
+
+import tw.brad.apis.MyPanel;
+
+public class MySign extends JFrame{
+	private MyPanel myPanel;
+	
+	public MySign() {
+		super("簽名");
+		
+		myPanel = new MyPanel();
+		setLayout(new BorderLayout());
+		add(myPanel, BorderLayout.CENTER);
+		
+		
+		setSize(800,  600);
+		setVisible(true);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+	}
+	
+	
 	public static void main(String[] args) {
-
+		new MySign();
 	}
 
 }
