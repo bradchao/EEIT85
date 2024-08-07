1a1e42ebf53622e25b7bb1a0a8e8e527f988b750
User <User@10.0.100.157>
2024-08-07 16:30:46 +0800

    08061630

--- BradJava/src/tw/brad/tutor/Brad48.java
+++ BradJava/src/tw/brad/tutor/Brad48.java
@@ -6,13 +6,13 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.InputStreamReader;
 
-public class Brad46 {
+public class Brad48 {
 
 	public static void main(String[] args) {
-		try {
-			FileInputStream fin = new FileInputStream("dir1/ns1hosp.csv");
-			InputStreamReader reader = new InputStreamReader(fin);
-			BufferedReader br = new BufferedReader(reader);
+		try(FileInputStream fin = new FileInputStream("dir1/ns1hosp.csv");
+				InputStreamReader reader = new InputStreamReader(fin);
+				BufferedReader br = new BufferedReader(reader);) {
+			
 			
 			String line;
 			while ( (line = br.readLine()) != null) {
@@ -21,7 +21,6 @@ public class Brad46 {
 				System.out.printf("%s:%s:%s:%s\n", data[1], data[2], data[4], data[7]);
 			}
 			
-			br.close();
 			System.out.println("OK");
 		} catch (Exception e) {
 			System.out.println(e);
