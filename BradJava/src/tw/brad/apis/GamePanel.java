d8920cf2573969136846f69efb3b75e52299d4c9
User <User@10.0.100.157>
2024-08-12 15:22:24 +0800

    08121522

---
+++ BradJava/src/tw/brad/apis/GamePanel.java
@@ -0,0 +1,12 @@
+package tw.brad.apis;
+
+import java.awt.Color;
+
+import javax.swing.JPanel;
+
+public class GamePanel extends JPanel {
+	public GamePanel() {
+		setBackground(Color.YELLOW);
+	}
+
+}
