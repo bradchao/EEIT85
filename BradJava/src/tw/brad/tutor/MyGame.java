fff682a7b9c2eae5e0ff8519196011866e8b5c17
User <User@10.0.100.157>
2024-08-13 09:43:00 +0800

    08130942

--- BradJava/src/tw/brad/tutor/MyGame.java
+++ BradJava/src/tw/brad/tutor/MyGame.java
@@ -5,14 +5,15 @@ import java.awt.BorderLayout;
 import javax.swing.JFrame;
 
 import tw.brad.apis.GamePanel;
+import tw.brad.apis.GamePanelV2;
 
 public class MyGame extends JFrame {
-	private GamePanel panel;
+	private GamePanelV2 panel;
 	
 	public MyGame() {
 	
 		setLayout(new BorderLayout());
-		panel = new GamePanel();
+		panel = new GamePanelV2();
 		add(panel, BorderLayout.CENTER);
 		
 		setSize(640, 480);
