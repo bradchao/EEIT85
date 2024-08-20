a9795625fe10f5fbf1e6367450a3f2994074cbf8
User <User@10.0.100.157>
2024-08-20 10:22:52 +0800

    08201022

--- BradWeb/src/main/java/tw/brad/javaee/Brad07.java
+++ BradWeb/src/main/java/tw/brad/javaee/Brad07.java
@@ -13,16 +13,33 @@ import javax.servlet.http.HttpServletResponse;
 public class Brad07 extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) 
 			throws ServletException, IOException {
+
+		String x, y, result;
+		x = y = result = "";
+		
+		request.setCharacterEncoding("UTF-8");
+		if (request.getParameter("x") != null) {
+			x = request.getParameter("x");
+			y = request.getParameter("y");
+		
+			try {
+				int r = Integer.parseInt(x) + Integer.parseInt(y);
+				result += r;
+			}catch(Exception e) {
+				
+			}
+		}
+		
 		
 		response.setContentType("text/html; charset=UTF-8");
 		PrintWriter out = response.getWriter();
 		out.println("<meta charset='UTF-8'>");
-		out.println("<form action='Brad06'>");
-		out.println("<input name='x' />");
+		out.println("<form action='Brad07'>");
+		out.printf("<input name='x' value='%s' />\n", x);
 		out.println("+");
-		out.println("<input name='y' />");
+		out.printf("<input name='y' value='%s' />\n", y);
 		out.println("<input type='submit' value='=' />");
-		out.println("<span></span>");
+		out.printf("<span>%s</span>\n", result);
 		out.println("</form>");
 		
 		response.flushBuffer();
