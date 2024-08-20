a1bc7682ad7019a921137c0fda5e30ddb0908ecb
User <User@10.0.100.157>
2024-08-20 16:30:03 +0800

    08201629

---
+++ BradWeb/src/main/java/tw/brad/javaee/Brad13.java
@@ -0,0 +1,25 @@
+package tw.brad.javaee;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/Brad13")
+public class Brad13 extends HttpServlet {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.setCharacterEncoding("UTF-8");
+		
+		RequestDispatcher dispatcher = request.getRequestDispatcher("Brad14");
+		
+		response.setContentType("text/html; charset=UTF-8");
+		response.getWriter().print("Hello, I am Brad13");
+		dispatcher.forward(request, response);
+		response.getWriter().print("after");
+	}
+
+}
