f689da8f4414afd2ea6a19c07285535485fd5226
User <User@10.0.100.157>
2024-08-20 11:27:30 +0800

    08201127

--- BradWeb/src/main/java/tw/brad/javaee/Brad07.java
+++ BradWeb/src/main/java/tw/brad/javaee/Brad07.java
@@ -14,20 +14,44 @@ public class Brad07 extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) 
 			throws ServletException, IOException {
 
-		String x, y, result;
-		x = y = result = "";
+		String x, y, result, op;
+		x = y = result = op = "";
 		
 		request.setCharacterEncoding("UTF-8");
 		if (request.getParameter("x") != null) {
 			x = request.getParameter("x");
 			y = request.getParameter("y");
-		
+			op = request.getParameter("op");
+			
 			try {
-				int r = Integer.parseInt(x) + Integer.parseInt(y);
-				result += r;
+				int intX = Integer.parseInt(x);
+				int intY = Integer.parseInt(y);
+			
+				int r1, r2;
+				
+				switch (op) {
+					case "1":
+						r1 = intX + intY;
+						result += r1;
+						break;
+					case "2":
+						r1 = intX - intY;
+						result += r1;
+						break;
+					case "3":
+						r1 = intX * intY;
+						result += r1;
+						break;
+					case "4":
+						r1 = intX / intY;
+						r2 = intX % intY;
+						result += String.format("%d ...... %d", r1, r2);
+						break;
+				}
 			}catch(Exception e) {
 				
 			}
+			
 		}
 		
 		
@@ -35,9 +59,14 @@ public class Brad07 extends HttpServlet {
 		PrintWriter out = response.getWriter();
 		out.println("<meta charset='UTF-8'>");
 		out.println("<form action='Brad07'>");
-		out.printf("<input name='x' value='%s' />\n", x);
-		out.println("+");
-		out.printf("<input name='y' value='%s' />\n", y);
+		out.printf("<input type='number' name='x' value='%s' />\n", x);
+		out.println("<select name='op'>");
+		out.printf("<option value='1' %s>+</option>\n", op.equals("1")?"selected":"");
+		out.printf("<option value='2' %s>-</option>\n", op.equals("2")?"selected":"");
+		out.printf("<option value='3' %s>x</option>\n", op.equals("3")?"selected":"");
+		out.printf("<option value='4' %s>/</option>\n", op.equals("4")?"selected":"");
+		out.println("</select>");
+		out.printf("<input type='number' name='y' value='%s' />\n", y);
 		out.println("<input type='submit' value='=' />");
 		out.printf("<span>%s</span>\n", result);
 		out.println("</form>");
