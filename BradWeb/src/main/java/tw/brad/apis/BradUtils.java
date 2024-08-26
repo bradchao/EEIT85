934debdda581e482862c5be373c2bb513692fb59
User <User@10.0.100.157>
2024-08-26 12:10:17 +0800

    08281210

--- BradWeb/src/main/java/tw/brad/apis/BradUtils.java
+++ BradWeb/src/main/java/tw/brad/apis/BradUtils.java
@@ -3,6 +3,11 @@ package tw.brad.apis;
 import java.io.BufferedInputStream;
 import java.io.File;
 import java.io.FileInputStream;
+import java.util.HashMap;
+import java.util.LinkedList;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 public class BradUtils {
 	// view => view1, view2, ....
@@ -43,6 +48,27 @@ public class BradUtils {
 	public static String createScore() {
 		return (int)(Math.random()*101) + "";
 	}
+	
+	public static LinkedList<HashMap<String, String>> parseFood(String json) {
+		LinkedList<HashMap<String, String>> items = new LinkedList<>();
+		JSONArray root = new JSONArray(json);
+		System.out.println(root.length());
+		for (int i=0; i<root.length(); i++) {
+			JSONObject row = root.getJSONObject(i);
+			HashMap<String, String> item = new HashMap<>();
+			item.put("aid", row.getString("ID"));
+			item.put("aname", row.getString("Name"));
+			item.put("acity", row.getString("City"));
+			item.put("atown", row.getString("Town"));
+			item.put("apicurl", row.getString("PicURL"));
+			item.put("alat", row.getString("Latitude"));
+			item.put("alng", row.getString("Longitude"));
+			items.add(item);
+		}
+		
+		return items;
+	}
+	
 	
 	
 }
