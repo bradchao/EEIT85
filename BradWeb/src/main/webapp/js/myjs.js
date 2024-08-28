b6647a81b3fcc3fb7a7deee589982b13382a074f
User <User@10.0.100.157>
2024-08-28 10:54:32 +0800

    08281054

--- BradWeb/src/main/webapp/js/myjs.js
+++ BradWeb/src/main/webapp/js/myjs.js
@@ -8,22 +8,41 @@ window.onload = function(){
 	let webSocket;
 	
 	start.addEventListener('click', function(){
-		webSocket = new WebSocket("ws://localhost:8080/BradWeb/myserver");
-		webSocket.onopen = function(){
-			console.log("opOpen");
-		};
-		webSocket.onerror = function(){
-			console.log("opError");
-		};
-		
+		connect("ws://localhost:8080/BradWeb/myserver");
 	});
 	
 	send.addEventListener('click', function(){
-		
+		let message = {
+			message: mesg.value 
+		};
+		webSocket.send(JSON.stringify(message));
 	});
 	
 	start.style.display = 'block';
 	mesgDiv.style.display = 'none';
+	
+	function connect(url){
+		webSocket = new WebSocket(url);
+		webSocket.onopen = function(){
+			console.log("onOpen");
+			
+			start.style.display = 'none';
+			mesgDiv.style.display = 'block';
+		};
+
+		webSocket.onclose = function(){
+			console.log("onClose");
+			start.style.display = 'block';
+			mesgDiv.style.display = 'none';
+		};
+
+
+		webSocket.onerror = function(){
+			console.log("onError");
+		};
+		
+	}
+	
 	
 	
 }
